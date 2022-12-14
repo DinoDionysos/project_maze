d927a0458c4382836f160eee60fa08f4082685a4
DinoDionysos <ai21m014@technikum-wien.at>
2022-12-15 00:06:21 +0100

    solves the maze and shows the paths

--- solve_maze_with_bfs.py
+++ solve_maze_with_bfs.py
@@ -6,6 +6,10 @@ import torch
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import collections
+import numpy as np
+
+# set seed for numpy shuffle
+np.random.seed(1)
 
 height = 7
 width = 7
@@ -19,10 +23,13 @@ m.end = (height-2, width-2)
 m.grid[m.start[0],m.start[1]] = 2
 m.grid[m.end[0],m.end[1]] = 3
 
-def showPNG(grid):
+
+cmap1 = ListedColormap(['white', 'black', (0.4,1,0.2), 'red'])
+cmap2 = ListedColormap(['white', 'black', (0.4,1,0.2), 'red', (0,0.6,1)])
+
+def showPNG(grid, cmap=None):
     """Generate a simple image of the maze."""
     fig = plt.figure(figsize=(10, 5))
-    cmap = ListedColormap(['white', 'black', (0.4,1,0.2), 'red'])#, (0,0.6,1)])
     plt.imshow(grid, cmap=cmap, interpolation='nearest')
     plt.xticks([]), plt.yticks([])
     plt.show()
@@ -31,19 +38,41 @@ def bfs(grid, start):
     queue = collections.deque()
     queue.append(start)
     seen = set([start])
+    # make two dim datastructur of size of the maze to store touples of x,y coordinates
+    successor = [[0 for x in range(width)] for y in range(height)]
+    path = []
     while queue:
-        path = queue.popleft()
-        x, y = path
-        if grid[y][x] == goal:
-            return True
+        next_step = queue.popleft()
+        # print(next_step)
+        x, y = next_step
+        if grid[y][x] == 3:
+            # do backtracking to find the path
+            while (x,y) != m.start:
+                path.append((x,y))
+                x,y = successor[x][y]
+            path.append(m.start)
+            path.reverse()
+            return True, path
         for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)): #directions
             if ( 0 <= x2 < width and  #X-axis in range
                 0 <= y2 < height and  #y-axis
-                grid[y2][x2] != wall and  #not a wall
+                grid[x2][y2] != 1 and  #not a wall
                 (x2, y2) not in seen): #not visited
                 queue.append( (x2, y2))
                 seen.add((x2, y2))
-    return False
+                successor[x2][y2] = (x,y)
 
+    return False , path
 
-showPNG(m.grid)
+print(m.grid)
+showPNG(m.grid, cmap1)
+success, path = bfs(m.grid, m.start)
+for i,j in path:
+    if (i,j) == m.start or (i,j) == m.end:
+        continue
+    m.grid[i,j] = 4
+
+print(m.grid)
+showPNG(m.grid, cmap2)
+
+print(success)
