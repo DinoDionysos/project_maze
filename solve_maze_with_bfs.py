7e3fa6ef8843478fd3dd8f2250f62e29c5d379a4
DinoDionysos <ai21m014@technikum-wien.at>
2022-12-15 00:32:34 +0100

    improve visuals

--- solve_maze_with_bfs.py
+++ solve_maze_with_bfs.py
@@ -9,23 +9,30 @@ import collections
 import numpy as np
 
 # set seed for numpy shuffle
-np.random.seed(1)
+# np.random.seed(2)
 
-height = 7
-width = 7
-assert height%2 == 1 and width%2==1, "height and width have to be uneven"
+side_length_square = 50
+height = side_length_square
+width = side_length_square
+if height%2 == 0:
+    height += 1
+    print("height has to be uneven, adding 1 to height")
+if width%2==0:
+    width += 1
+    print("width has to be uneven, adding 1 to width")
+
 m = Maze()
 # m.generator = Prims((height-1)/2, (width-1)/2)
 m.generator = Kruskal((height-1)/2, (width-1)/2)
 m.generate()
 m.start = (1, 1)
 m.end = (height-2, width-2)
-m.grid[m.start[0],m.start[1]] = 2
-m.grid[m.end[0],m.end[1]] = 3
+m.grid[m.start[1],m.start[0]] = 2
+m.grid[m.end[1],m.end[0]] = 3
 
 
-cmap1 = ListedColormap(['white', 'black', (0.4,1,0.2), 'red'])
-cmap2 = ListedColormap(['white', 'black', (0.4,1,0.2), 'red', (0,0.6,1)])
+cmap1 = ListedColormap(['white', 'black', (0.4,1,0.2), 'blue'])
+cmap2 = ListedColormap(['white', 'black', (0.4,1,0.2), 'blue', (0,0.6,1), 'red'])
 
 def showPNG(grid, cmap=None):
     """Generate a simple image of the maze."""
@@ -55,37 +62,39 @@ def bfs(grid, start):
     while queue:
         next_step = queue.popleft()
         # print(next_step)
-        x, y = next_step
+        y, x = next_step
         if grid[y][x] == 3:
             # do backtracking to find the path
-            while (x,y) != m.start:
-                path.append((x,y))
-                x,y = successor[x][y]
+            while (y,x) != m.start:
+                if grid[y,x] != 3:
+                    grid[y,x] = 5
+                path.append((y,x))
+                y,x = successor[y][x]
             path.append(m.start)
             path.reverse()
             return True, path
-        for x2, y2 in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)): #directions
+        for y2, x2 in ((y+1,x), (y-1,x), (y,x+1), (y,x-1)): #directions
             if ( 0 <= x2 < width and  #X-axis in range
                 0 <= y2 < height and  #y-axis
-                grid[x2][y2] != 1 and  #not a wall
-                (x2, y2) not in seen): #not visited
-                queue.append( (x2, y2))
-                seen.add((x2, y2))
-                successor[x2][y2] = (x,y)
-
+                grid[y2][x2] != 1 and  #not a wall
+                (y2, x2) not in seen): #not visited
+                queue.append( (y2, x2))
+                seen.add((y2, x2))
+                successor[y2][x2] = (y,x)
+                if grid[y2,x2] != 3:
+                    grid[y2,x2] = 4
     return False , path
 
 
 import copy
 save_grid = copy.deepcopy(m.grid)
-
+save_grid[0,0] = 0
+print(save_grid)
 # solve mazeand do backtracking
 success, path = bfs(m.grid, m.start)
-for i,j in path:
-    if (i,j) == m.start or (i,j) == m.end:
-        continue
-    m.grid[i,j] = 4
 
+print(m.grid)
+m.grid[0,0] = 0
 show2PNG(save_grid, m.grid, cmap1, cmap2)
 
 print(success)
