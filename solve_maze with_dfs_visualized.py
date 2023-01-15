b38431c67649f5a63c204fd98b53de3d701ea6f1
alexanderzulechner <alexander.zulechner@craftworks.at>
2023-01-15 15:57:46 +0100

    linting and formatting + suppport for macOS MPS as GPU pytorch run env

--- solve_maze with_dfs_visualized.py
+++ solve_maze with_dfs_visualized.py
@@ -1,13 +1,9 @@
-from mazelib.generate.Prims import Prims
-from mazelib.generate.Kruskal import Kruskal
-from mazelib import Maze
-from mazelib.solve.BacktrackingSolver import BacktrackingSolver
-import torch
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 import collections
-import numpy as np
 import tkinter as tk
+
+import numpy as np
+from mazelib import Maze
+from mazelib.generate.Kruskal import Kruskal
 
 from util import draw_maze
 
@@ -19,21 +15,22 @@ size_rectangles = 15
 side_length_square = 31
 height = side_length_square
 width = side_length_square
-assert height%2 == 1 and width%2==1, "height and width have to be uneven"
+assert height % 2 == 1 and width % 2 == 1, "height and width have to be uneven"
 m = Maze()
-# m.generator = Prims((height-1)/2, (width-1)/2)
-m.generator = Kruskal((height-1)/2, (width-1)/2)
+# m.generator = Prims(int((height-1)/2), int((width-1)/2))
+m.generator = Kruskal(int((height - 1) / 2), int((width - 1) / 2))
 m.generate()
 m.start = (1, 1)
-m.end = (height-2, width-2)
-m.grid[m.start[1],m.start[0]] = 2
-m.grid[m.end[1],m.end[0]] = 3
+m.end = (height - 2, width - 2)
+m.grid[m.start[1], m.start[0]] = 2
+m.grid[m.end[1], m.end[0]] = 3
 
 # make a tkinter window with a canvas that will show the maze
 root = tk.Tk()
-canvas = tk.Canvas(root, width=width*size_rectangles, height=height*size_rectangles)
+canvas = tk.Canvas(root, width=width * size_rectangles, height=height * size_rectangles)
 canvas.pack()
 fps = 2
+
 
 def dfs(grid, start):
     stack = collections.deque()
@@ -47,30 +44,31 @@ def dfs(grid, start):
         # print(next_step)
         y, x = next_step
         seen.add((y, x))
-        if grid[y,x] != 3 and grid[y,x] != 2:
-            grid[y,x] = 4
+        if grid[y, x] != 3 and grid[y, x] != 2:
+            grid[y, x] = 4
         if grid[y][x] == 3:
             # do backtracking to find the path
-            while (y,x) != m.start:
-                if grid[y,x] != 3:
-                    grid[y,x] = 5
-                path.append((y,x))
-                y,x = successor[y][x]
+            while (y, x) != m.start:
+                if grid[y, x] != 3:
+                    grid[y, x] = 5
+                path.append((y, x))
+                y, x = successor[y][x]
             path.append(m.start)
             path.reverse()
             draw_maze(grid, canvas, size_rectangles)
             return True, path
-        for y2, x2 in ((y+1,x), (y-1,x), (y,x+1), (y,x-1)): #directions
-            if ( 0 <= x2 < width and  #X-axis in range
-                0 <= y2 < height and  #y-axis
-                grid[y2][x2] != 1 and  #not a wall
-                (y2, x2) not in seen): #not visited
-                stack.append( (y2, x2))
-                successor[y2][x2] = (y,x)
+        for y2, x2 in ((y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)):  # directions
+            if (0 <= x2 < width and  # X-axis in range
+                    0 <= y2 < height and  # y-axis
+                    grid[y2][x2] != 1 and  # not a wall
+                    (y2, x2) not in seen):  # not visited
+                stack.append((y2, x2))
+                successor[y2][x2] = (y, x)
                 draw_maze(grid, canvas, size_rectangles)
-                #make a pause for 0.5 seconds
+                # make a pause for 0.5 seconds
                 # root.after(round(1000/fps))
-    return False , path
+    return False, path
+
 
 success, path = dfs(m.grid, m.start)
 
@@ -78,4 +76,3 @@ print(success)
 print(path)
 
 root.mainloop()
-
--- test_mazelib.py
+++ test_mazelib.py
@@ -2,28 +2,27 @@
 # try the syntax to generate a maze
 # print some meta information
 
-from mazelib.generate.Prims import Prims
-from mazelib.generate.Kruskal import Kruskal
-from mazelib import Maze
-from mazelib.solve.BacktrackingSolver import BacktrackingSolver
-import torch
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from mazelib import Maze
+from mazelib.generate.Kruskal import Kruskal
 
 height = 7
 width = 7
-assert height%2 == 1 and width%2==1, "height and width have to be uneven"
+assert height % 2 == 1 and width % 2 == 1, "height and width have to be uneven"
 # assert w >= 3 and h >= 3, "Mazes cannot be smaller than 3x3."
 m = Maze()
 # m.generator = Prims(height, width)
-m.generator = Kruskal((height-1)/2, (width-1)/2)
+m.generator = Kruskal(int((height - 1) / 2), int((width - 1) / 2))
 # m.solver = BacktrackingSolver()
 # # make difficult maze (hard = long solution path), by taking 100 mazes, solve them 10 times with varying end and start points and take the one with the longest solution path
 # m.generate_monte_carlo(100, 10, 1.0)
 m.generate()
 m.start = (1, 1)
 # m.end = (2*height-1, 2*width-1)
-m.end = (height-2, width-2)
+m.end = (height - 2, width - 2)
+
+
 # m.generate_entrances()
 # m.generate_entrances(start_outer=True, end_outer=True)
 
@@ -31,13 +30,14 @@ m.end = (height-2, width-2)
 def showPNG(grid):
     """Generate a simple image of the maze."""
     fig = plt.figure(figsize=(10, 5))
-    cmap = ListedColormap(['white', 'black', (0.4,1,0.2), 'red'])#, (0,0.6,1)])
+    cmap = ListedColormap(['white', 'black', (0.4, 1, 0.2), 'red'])  # , (0,0.6,1)])
     plt.imshow(grid, cmap=cmap, interpolation='nearest')
     plt.xticks([]), plt.yticks([])
     plt.show()
 
-m.grid[m.start[0],m.start[1]] = 2
-m.grid[m.end[0],m.end[1]] = 3
+
+m.grid[m.start[0], m.start[1]] = 2
+m.grid[m.end[0], m.end[1]] = 3
 print(m.grid)
 print(type(m.grid))
 print(m.start)
